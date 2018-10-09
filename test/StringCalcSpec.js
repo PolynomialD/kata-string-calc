da368c3ff40ec45289ab31cafc53297d5b5c312a
PolynomialD <steven.grindey@gmail.com>
2018-10-09 12:03:17 +0100

    2nd test failing

--- test/StringCalcSpec.js
+++ test/StringCalcSpec.js
@@ -5,4 +5,11 @@ describe('StringCalc', () => {
     const stringCalc = new StringCalc()
     stringCalc.add('').should.eql(0)
   })
+
+  verify.it('A single number returns the value', () => {
+    const stringCalc = new StringCalc()
+    stringCalc.add(1).should.eql(1)
+
+  })
+
 })
