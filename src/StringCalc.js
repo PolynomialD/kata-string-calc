3b8254f5ff9cfed289c313509bfff1e566816a29
PolynomialD <steven.grindey@gmail.com>
2018-10-10 19:39:48 +0100

    refactored some more

--- src/StringCalc.js
+++ src/StringCalc.js
@@ -5,9 +5,12 @@ class StringCalc {
     let negArray = array.filter(value => value < 0)
     let oneKArray = array.filter(value => value < 1001, 0)
 
-    if(dString === '' || oneKArray.length === 0) return 0
-    if (negArray.length > 0) {return 'negatives not allowed: ' + negArray.toString()}
-    else return oneKArray.reduce((a, b) => {return a + b})
+    if (dString === '' || oneKArray.length === 0) return 0
+    if (negArray.length > 0) {
+      return 'negatives not allowed: ' + negArray.toString()
+    } else {
+        return oneKArray.reduce((a, b) =>  a + b)
+      }
   }
 }
 module.exports = StringCalc
