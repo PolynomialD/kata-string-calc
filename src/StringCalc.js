81be918d0d8e411d03d8bfdfedd6f38e2fcd7016
PolynomialD <steven.grindey@gmail.com>
2018-10-10 13:27:07 +0100

    5th test passed

--- src/StringCalc.js
+++ src/StringCalc.js
@@ -1,18 +1,9 @@
 class StringCalc {
 
   add (dString) {
-    let array = []
     if(dString === '') return 0
-    if (dString.includes(',')){
-      array = dString.split(',').map(Number)
-      return array.reduce((a, b) => {return a+b})
-    }
-    if (dString.includes('\n')){
-      array = dString.split('\n').map(Number)
-      return array.reduce((a, b) => {return a+b})
-    } else {
-        return parseInt(dString)
-      }
+    let array = dString.split('\n').join().split(',').map(Number)
+    return array.reduce((a, b) => {return a+b})
   }
 }
 module.exports = StringCalc
