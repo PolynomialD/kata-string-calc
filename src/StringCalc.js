d872e8e2599540a08c650764b360d9705d51c367
PolynomialD <steven.grindey@gmail.com>
2018-10-12 00:43:17 +0100

    refactored some more

--- src/StringCalc.js
+++ src/StringCalc.js
@@ -7,15 +7,9 @@ class StringCalc {
     if (array.length === 0) return 0
     if (negArray.length > 0) throw new Error('negatives not allowed: ' + negArray.toString())
     if(dString.startsWith('//')) {
-      let newDelim = ''
-      for(let i=2; isNaN(dString[i]); i++) {
-        if (isNaN(dString[i])) {newDelim += dString[i]}
-      }
-      console.log(array)
-      array.splice(0,1)
-      console.log(array)
+      let newDelim = array.splice(0,1).toString().slice(2)
       array = array.join('').split(newDelim)
-    }
+    } 
     return array.map(Number).reduce((a, b) => a + b)
   }
   catch(error) {console.log(error)}
