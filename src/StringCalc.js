e2983827db9e72c52fbe5c0d5aa5424813aaad06
PolynomialD <steven.grindey@gmail.com>
2018-10-11 20:00:32 +0100

    refactored

--- src/StringCalc.js
+++ src/StringCalc.js
@@ -2,30 +2,16 @@ class StringCalc {
 
   add (dString) {
     let array = dString.split('\n').join().split(',')
-    console.log('array:', array)
     let negArray = array.filter(value => value < 0)
-    console.log('negArray:', negArray)
-
     let newDelimString = dString.substring(0,2)
 
     if(newDelimString === '//') {
       let newDelim = dString[2]
-      console.log('newDelim:', newDelim)
-      let string = array.splice(0,1).join()
-      array = array.join().split('')
-      console.log('string:', string)
-      console.log('array:', array)
-
+      array.splice(0,1).join()
+      array = array.join().split(newDelim)
     }
     array = array.map(Number)
-    console.log('array:', array)
-
     let oneKArray = array.filter(value => value < 1001, 0)
-
-    console.log('oneKArray:', oneKArray)
-    console.log('dString:', dString)
-    console.log('newDelimString:', newDelimString)
-
 
     if (dString === '' || oneKArray.length === 0) return 0
     if (negArray.length > 0) {
