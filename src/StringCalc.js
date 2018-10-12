48cd0aba2cc95765d760bf2438b07960f7d8259d
PolynomialD <steven.grindey@gmail.com>
2018-10-12 02:13:04 +0100

    refactored again

--- src/StringCalc.js
+++ src/StringCalc.js
@@ -2,10 +2,10 @@ class StringCalc {
 
   add (dString) {
     let array = dString.split('\n').join().split(',').filter(i => isNaN(i) || i <= 1000)
-    let negArray = array.filter(value => value < 0)
+    let negatives = array.filter(i => i < 0).toString()
 
-    if (array.length === 0) return 0
-    if (negArray.length > 0) throw new Error('negatives not allowed: ' + negArray.toString())
+    if(array.length === 0) return 0
+    if (negatives) throw new Error('negatives not allowed: ' + negatives)
     if(dString.startsWith('//')) {
       let newDelim = array.splice(0,1).toString().slice(2)
       array = array.join('').split(newDelim)
